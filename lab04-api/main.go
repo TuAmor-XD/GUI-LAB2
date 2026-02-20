99b82e53b2972cb842618ccc2b4f5aee0b5f70d2
TuAmor-XD <2021256010@ub.edu.bz>
2026-02-20 09:04:25 -0600

    Part 2 Completed: Implement the custom Response Writer

--- lab04-api/main.go
+++ lab04-api/main.go
@@ -8,3 +8,12 @@ import (
 	"time"
 )
 
+type responseWriter struct {
+	http.ResponseWriter
+	statusCode int
+}
+
+func (rw *responseWriter) WriteHeader(code int) {
+	rw.statusCode = code
+	rw.ResponseWriter.WriteHeader(code)
+}
